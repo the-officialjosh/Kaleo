43efa3c2cebf0c41e26705575ee4d18b909261bd
Joshua Onyema <onyemajoshua892@gmail.com>
2026-01-04 06:27:37 -0500

    add QrCode entity with fields for ID, status, value, associated RegistrationPass, and auditing support
    Signed-off-by: Joshua Onyema <onyemajoshua892@gmail.com>

---
+++ src/main/java/dev/joshuaonyema/kaleo/domain/QrCode.java
@@ -0,0 +1,44 @@
+package dev.joshuaonyema.kaleo.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "qr_codes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class QrCode {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private QrCodeStatusEnum status;
+
+    @Column(name = "value", nullable = false)
+    private String value;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pass_id")
+    private RegistrationPass pass;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
