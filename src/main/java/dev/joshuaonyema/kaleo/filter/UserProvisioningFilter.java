662dcba7ebb27ac2e2f38201dc3c3b13a87a0271
Joshua Onyema <onyemajoshua892@gmail.com>
2026-01-04 08:23:48 -0500

    add UserProvisioningFilter to handle user provisioning from JWT authentication
    Signed-off-by: Joshua Onyema <onyemajoshua892@gmail.com>

---
+++ src/main/java/dev/joshuaonyema/kaleo/filter/UserProvisioningFilter.java
@@ -0,0 +1,49 @@
+package dev.joshuaonyema.kaleo.filter;
+
+import dev.joshuaonyema.kaleo.domain.User;
+import dev.joshuaonyema.kaleo.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+
+@Component
+@RequiredArgsConstructor
+public class UserProvisioningFilter extends OncePerRequestFilter {
+
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null
+                && authentication.isAuthenticated()
+                && authentication.getPrincipal() instanceof Jwt jwt){
+            UUID keyCloakId = UUID.fromString(jwt.getSubject());
+
+            if (!userRepository.existsById(keyCloakId)){
+                User user = new User();
+                user.setId(keyCloakId);
+                user.setName(jwt.getClaimAsString("preferred_username"));
+                user.setEmail(jwt.getClaimAsString("email"));
+                userRepository.save(user);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
