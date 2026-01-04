eebe0288b3bf3ae49ba3d373995103f92fa3ace6
Joshua Onyema <onyemajoshua892@gmail.com>
2026-01-04 08:23:55 -0500

    add SecurityConfig to configure HTTP security and integrate UserProvisioningFilter
    Signed-off-by: Joshua Onyema <onyemajoshua892@gmail.com>

---
+++ src/main/java/dev/joshuaonyema/kaleo/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package dev.joshuaonyema.kaleo.config;
+
+import dev.joshuaonyema.kaleo.filter.UserProvisioningFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    Customizer<HttpSecurity> httpSecurityCustomizer(UserProvisioningFilter userProvisioningFilter){
+        return httpSecurity -> httpSecurity
+                .authorizeHttpRequests(
+                        authorize->authorize
+                                .anyRequest()
+                                .authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(
+                        session-> session
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterAfter(userProvisioningFilter, BearerTokenAuthenticationFilter.class);
+    }
+}
