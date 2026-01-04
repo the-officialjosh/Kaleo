482543f2b1fd0423b4a907bac410552d7fe42417
Joshua Onyema <onyemajoshua892@gmail.com>
2026-01-04 05:59:12 -0500

    add RegistrationPass entity with fields for ID, status, pass type, registrant, and auditing support
    Signed-off-by: Joshua Onyema <onyemajoshua892@gmail.com>

---
+++ src/main/java/dev/joshuaonyema/kaleo/domain/RegistrationPass.java
@@ -0,0 +1,49 @@
+package dev.joshuaonyema.kaleo.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "registration_passes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class RegistrationPass {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private RegistrationPassStatusEnum registrationPassStatusEnum;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pass_type_id")
+    private RegistrationPassType passType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "registrant_id")
+    private User registrant;
+
+    // TODO: Validation
+
+    // TODO: QRCode
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
