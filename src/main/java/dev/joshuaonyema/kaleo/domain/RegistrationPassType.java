2c3bb172b7cea3622f42bcfa82fc300bfa8fdc72
Joshua Onyema <onyemajoshua892@gmail.com>
2026-01-04 05:42:15 -0500

    add RegistrationPassType entity with fields for ID, name, price, availability, and auditing support
    Signed-off-by: Joshua Onyema <onyemajoshua892@gmail.com>

---
+++ src/main/java/dev/joshuaonyema/kaleo/domain/RegistrationPassType.java
@@ -0,0 +1,48 @@
+package dev.joshuaonyema.kaleo.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "registration_pass_type")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class RegistrationPassType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "total_available")
+    private Integer totalAvailable;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id")
+    private Program program;
+
+    // TODO: REGISTRATION PASS
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
