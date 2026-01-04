6ebf258acb3eaeb7ffff800d0b191190cadf5ffe
Joshua Onyema <onyemajoshua892@gmail.com>
2026-01-04 01:49:26 -0500

    initialize Kaleo project with Spring Boot and Maven setup

---
+++ src/main/java/dev/joshuaonyema/kaleo/KaleoApplication.java
@@ -0,0 +1,13 @@
+package dev.joshuaonyema.kaleo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaleoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaleoApplication.class, args);
+    }
+
+}
