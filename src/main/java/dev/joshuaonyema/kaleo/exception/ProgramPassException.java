fbfce5e8947bdc851dbd9c97a5ed186470a9145d
Joshua Onyema <onyemajoshua892@gmail.com>
2026-01-05 03:25:24 -0500

    add ProgramPassException class for handling program pass-related errors
    Signed-off-by: Joshua Onyema <onyemajoshua892@gmail.com>

---
+++ src/main/java/dev/joshuaonyema/kaleo/exception/ProgramPassException.java
@@ -0,0 +1,24 @@
+package dev.joshuaonyema.kaleo.exception;
+
+
+public class ProgramPassException extends RuntimeException{
+
+    public ProgramPassException() {
+    }
+
+    public ProgramPassException(String message) {
+        super(message);
+    }
+
+    public ProgramPassException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ProgramPassException(Throwable cause) {
+        super(cause);
+    }
+
+    public ProgramPassException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
