fed19a1a03b6309a96ddf42316e233e6316f29d3
Joshua Onyema <onyemajoshua892@gmail.com>
2026-01-06 05:02:32 -0500

    add: create ErrorDto to standardize error response structure
    Signed-off-by: Joshua Onyema <onyemajoshua892@gmail.com>

---
+++ src/main/java/dev/joshuaonyema/kaleo/api/dto/ErrorDto.java
@@ -0,0 +1,12 @@
+package dev.joshuaonyema.kaleo.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDto {
+    private String error;
+}
