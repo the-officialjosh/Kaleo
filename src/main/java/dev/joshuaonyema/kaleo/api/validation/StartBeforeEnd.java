c9650c6a8e37b93cdcf661bf1aa8986d15b94deb
Joshua Onyema <onyemajoshua892@gmail.com>
2026-01-06 00:29:17 -0500

    add StartBeforeEnd annotation for validation of start and end times
    Signed-off-by: Joshua Onyema <onyemajoshua892@gmail.com>

---
+++ src/main/java/dev/joshuaonyema/kaleo/api/validation/StartBeforeEnd.java
@@ -0,0 +1,21 @@
+package dev.joshuaonyema.kaleo.api.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = StartBeforeEndValidator.class)
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface StartBeforeEnd {
+    String message() default  "startTime must be before endTime";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+    String startField() default "startTime";
+    String endField() default "endTime";
+}
