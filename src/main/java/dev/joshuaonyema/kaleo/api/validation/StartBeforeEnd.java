ed4d1539b3642e4a323dd74b5e8ce07d4a202f34
Joshua Onyema <onyemajoshua892@gmail.com>
2026-01-06 00:39:20 -0500

    add support for repeatable StartBeforeEnd annotation
    Signed-off-by: Joshua Onyema <onyemajoshua892@gmail.com>

--- src/main/java/dev/joshuaonyema/kaleo/api/validation/StartBeforeEnd.java
+++ src/main/java/dev/joshuaonyema/kaleo/api/validation/StartBeforeEnd.java
@@ -8,6 +8,7 @@ import java.lang.annotation.*;
 @Documented
 @Constraint(validatedBy = StartBeforeEndValidator.class)
 @Target(ElementType.TYPE)
+@Repeatable(StartBeforeEnd.List.class)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface StartBeforeEnd {
     String message() default  "startTime must be before endTime";
@@ -18,4 +19,11 @@ public @interface StartBeforeEnd {
 
     String startField() default "startTime";
     String endField() default "endTime";
+
+    @Target(ElementType.TYPE)
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    @interface List {
+        StartBeforeEnd[] value();
+    }
 }
