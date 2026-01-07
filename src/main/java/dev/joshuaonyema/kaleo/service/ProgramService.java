8e0cc5e68689a0c5e15e8eae677ae06af6d766c8
Joshua Onyema <onyemajoshua892@gmail.com>
2026-01-07 00:14:18 -0500

    fix: add method to list events for organizer in ProgramService
    Signed-off-by: Joshua Onyema <onyemajoshua892@gmail.com>

--- src/main/java/dev/joshuaonyema/kaleo/service/ProgramService.java
+++ src/main/java/dev/joshuaonyema/kaleo/service/ProgramService.java
@@ -3,8 +3,9 @@ package dev.joshuaonyema.kaleo.service;
 
 import dev.joshuaonyema.kaleo.domain.entity.Program;
 import dev.joshuaonyema.kaleo.service.request.CreateProgramRequest;
+import org.springframework.data.domain.Page;
 
 public interface ProgramService {
     Program createProgram(CreateProgramRequest program);
-
+    Page<Program> listEventsForOrganizer();
 }
