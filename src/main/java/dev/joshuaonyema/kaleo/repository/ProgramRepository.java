e31c21c20568aadd3896d4cc9ee80bb1061b1f33
Joshua Onyema <onyemajoshua892@gmail.com>
2026-01-07 00:14:09 -0500

    fix: add pagination support for finding programs by organizer in ProgramRepository
    Signed-off-by: Joshua Onyema <onyemajoshua892@gmail.com>

--- src/main/java/dev/joshuaonyema/kaleo/repository/ProgramRepository.java
+++ src/main/java/dev/joshuaonyema/kaleo/repository/ProgramRepository.java
@@ -1,6 +1,9 @@
 package dev.joshuaonyema.kaleo.repository;
 
 import dev.joshuaonyema.kaleo.domain.entity.Program;
+import dev.joshuaonyema.kaleo.domain.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,5 @@ import java.util.UUID;
 
 @Repository
 public interface ProgramRepository extends JpaRepository<Program, UUID> {
+    Page<Program> findByOrganizer(User organizer, Pageable pageable);
 }
