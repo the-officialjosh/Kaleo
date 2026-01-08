57b273737b054cc69daa7d64a99af7e2650063a0
Joshua Onyema <onyemajoshua892@gmail.com>
2026-01-08 03:57:54 -0500

    feat: add response DTOs for published programs and pass types
    Signed-off-by: Joshua Onyema <onyemajoshua892@gmail.com>

---
+++ src/main/java/dev/joshuaonyema/kaleo/api/dto/response/GetPublishedProgramDetailsResponseDto.java
@@ -0,0 +1,23 @@
+package dev.joshuaonyema.kaleo.api.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetPublishedProgramDetailsResponseDto {
+    private UUID id;
+    private String name;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private String venue;
+    private List<GetPublishedPassTypesResponseDto> passTypes = new ArrayList<>();
+
+}
