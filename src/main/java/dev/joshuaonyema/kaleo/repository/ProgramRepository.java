06a32a44463c84e9d519c046b4de967df31e2b24
Joshua Onyema <onyemajoshua892@gmail.com>
2026-01-08 02:29:55 -0500

    feat: add search query for published programs in ProgramRepository
    Signed-off-by: Joshua Onyema <onyemajoshua892@gmail.com>

--- src/main/java/dev/joshuaonyema/kaleo/repository/ProgramRepository.java
+++ src/main/java/dev/joshuaonyema/kaleo/repository/ProgramRepository.java
@@ -6,6 +6,8 @@ import dev.joshuaonyema.kaleo.domain.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -16,4 +18,22 @@ public interface ProgramRepository extends JpaRepository<Program, UUID> {
     Page<Program> findByOrganizer(User organizer, Pageable pageable);
     Optional<Program> findByIdAndOrganizer(UUID id, User organizer);
     Page<Program> findByStatus(ProgramStatus status, Pageable pageable);
+
+    // This query is particular to PostgreSQL, might add room for upgrade to ElasticSearch
+    @Query(
+            value =
+                    "SELECT * FROM programs " +
+                            "WHERE status = 'PUBLISHED' " +
+                            "AND to_tsvector('english', " +
+                            "COALESCE(name, '') || ' ' || COALESCE(venue, '')) " +
+                            "@@ plainto_tsquery('english', :searchTerm)",
+            countQuery =
+                    "SELECT count(*) FROM programs " +
+                            "WHERE status = 'PUBLISHED' " +
+                            "AND to_tsvector('english', " +
+                            "COALESCE(name, '') || ' ' || COALESCE(venue, '')) " +
+                            "@@ plainto_tsquery('english', :searchTerm)",
+            nativeQuery = true
+    )
+    Page<Program> searchPrograms(@Param("searchTerm") String searchTerm, Pageable pageable);
 }
