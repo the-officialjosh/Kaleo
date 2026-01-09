bb193ae8f8da7b425a5f69dcf9e8f1dc96b2c1ad
Joshua Onyema <onyemajoshua892@gmail.com>
2026-01-08 23:10:17 -0500

    feat: refine pass purchase logic, add PassTypeController, and enhance repository with locking for concurrency
    Signed-off-by: Joshua Onyema <onyemajoshua892@gmail.com>

--- src/main/java/dev/joshuaonyema/kaleo/application/service/PassService.java
+++ src/main/java/dev/joshuaonyema/kaleo/application/service/PassService.java
@@ -1,9 +1,7 @@
 package dev.joshuaonyema.kaleo.application.service;
 
-import dev.joshuaonyema.kaleo.domain.entity.Pass;
-
 import java.util.UUID;
 
 public interface PassService {
-    Pass purchasePass(UUID ticketTypeId);
+    void purchasePass(UUID passTypeId);
 }
--- src/main/java/dev/joshuaonyema/kaleo/repository/PassTypeRepository.java
+++ src/main/java/dev/joshuaonyema/kaleo/repository/PassTypeRepository.java
@@ -1,11 +1,19 @@
 package dev.joshuaonyema.kaleo.repository;
 
 import dev.joshuaonyema.kaleo.domain.entity.PassType;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PassTypeRepository extends JpaRepository<PassType, UUID> {
+    @Query("SELECT pt FROM PassType pt WHERE pt.id = :id")
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<PassType> findByIdWithLock(@Param("id") UUID id);
 }
