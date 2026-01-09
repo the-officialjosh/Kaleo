551c93fe6941d882e5908ee7904e1d854862fec7
Joshua Onyema <onyemajoshua892@gmail.com>
2026-01-09 03:33:40 -0500

    feat: add method to find QR code by passId and registrantId in QrCodeRepository
    Signed-off-by: Joshua Onyema <onyemajoshua892@gmail.com>

--- src/main/java/dev/joshuaonyema/kaleo/repository/QrCodeRepository.java
+++ src/main/java/dev/joshuaonyema/kaleo/repository/QrCodeRepository.java
@@ -4,8 +4,10 @@ import dev.joshuaonyema.kaleo.domain.entity.QrCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface QrCodeRepository extends JpaRepository<QrCode, UUID> {
+    Optional<QrCode> findByPassIdAndPassRegistrantId(UUID passId, UUID registrantID);
 }
