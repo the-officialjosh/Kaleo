9dd80c5bc750e684e56ae2173afbfb42e7106518
Joshua Onyema <onyemajoshua892@gmail.com>
2026-01-09 02:04:27 -0500

    feat: add paginated retrieval of user passes in PassService and repository
    Signed-off-by: Joshua Onyema <onyemajoshua892@gmail.com>

--- src/main/java/dev/joshuaonyema/kaleo/application/service/PassService.java
+++ src/main/java/dev/joshuaonyema/kaleo/application/service/PassService.java
@@ -1,7 +1,12 @@
 package dev.joshuaonyema.kaleo.application.service;
 
+import dev.joshuaonyema.kaleo.domain.entity.Pass;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import java.util.UUID;
 
 public interface PassService {
     void purchasePass(UUID passTypeId);
+    Page<Pass> listPassesForUser(Pageable pageable);
 }
--- src/main/java/dev/joshuaonyema/kaleo/repository/PassRepository.java
+++ src/main/java/dev/joshuaonyema/kaleo/repository/PassRepository.java
@@ -1,6 +1,8 @@
 package dev.joshuaonyema.kaleo.repository;
 
 import dev.joshuaonyema.kaleo.domain.entity.Pass;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import java.util.UUID;
 @Repository
 public interface PassRepository extends JpaRepository<Pass, UUID> {
     int countByPassTypeId(UUID ticketTypeId);
+    Page<Pass> findByRegistrantId(UUID RegistrantId, Pageable pageable);
 }
